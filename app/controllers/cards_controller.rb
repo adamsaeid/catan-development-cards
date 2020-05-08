d66a29b8523ebc3dc2884425892abfc38f21a151
Adam Saeid <adam.saeid@gmail.com>
2020-05-09 00:45:56 +0100

    Add action to draw card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,6 @@
 class CardsController < ApplicationController
+  def draw
+    drawn_card = Card.where(player_id: nil).sample
+    drawn_card.update_attributes(player_id: params['player_id'])
+  end
 end
