d98944ab36dac5ab3f9b825e98d84c9cb63614ac
Adam Saeid <adam.saeid@gmail.com>
2020-05-09 02:29:41 +0100

    secure player page behind password

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,9 +3,14 @@ class PlayersController < ApplicationController
     drawn_card = Card.where(player_id: nil).sample
     drawn_card.update_attributes(player_id: params['player_id'])
   end
-  
+
   def show
     @player = Player.find(params['player_id'])
+
+    authenticate_or_request_with_http_basic do |name, password|
+      name == @player.name && password == @player.password
+    end
+
   end
 
   def index
