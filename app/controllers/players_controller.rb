44d62fd6c2066eccb22abb7f023dbf5bb3fdc144
Adam Saeid <adam.saeid@gmail.com>
2020-05-09 19:36:54 +0100

    Add ability to play card

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,6 +12,8 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params['player_id'])
 
+    @played_cards = Card.where.not(played_at: nil)
+
     authenticate_or_request_with_http_basic do |name, password|
       name == @player.name && password == @player.password
     end
