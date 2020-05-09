904b6007d8a33cef139c16cfd65f5adfa5def9a0
Adam Saeid <adam.saeid@gmail.com>
2020-05-09 02:13:44 +0100

    Add form to draw cards for players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,12 @@ class PlayersController < ApplicationController
     drawn_card = Card.where(player_id: nil).sample
     drawn_card.update_attributes(player_id: params['player_id'])
   end
+  
   def show
     @player = Player.find(params['player_id'])
+  end
+
+  def index
+    @players = Player.all
   end
 end
