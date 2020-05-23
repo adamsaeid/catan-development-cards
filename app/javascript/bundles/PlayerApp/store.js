f312413f5799386455e2d08d3e6c354f9b4e4569
Adam Saeid <adam.saeid@gmail.com>
2020-05-23 23:29:32 +0100

    Add redux thunk middleware

--- app/javascript/bundles/PlayerApp/store.js
+++ app/javascript/bundles/PlayerApp/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import resourcesReducer from './reducers/resourcesReducer';
 
-let store = createStore(resourcesReducer);
+let store = createStore(resourcesReducer, applyMiddleware(thunk));
 
 export default store;
