7d723b2d42cb4d1d0351117274326bb44b21e3ae
Adam Saeid <adam.saeid@gmail.com>
2020-05-25 21:21:05 +0100

    add webpack config for fonts

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -22,5 +22,18 @@ module.exports = {
       },
     ],
   },
-  ...
+  loaders: [
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      ]      
+    }
+  ]
 };
