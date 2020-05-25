d655a0aa7db0a56e9867c7a47c74945b8d8aa6ca
Adam Saeid <adam.saeid@gmail.com>
2020-05-25 22:04:52 +0100

    Include fonts in player app layout

--- app/javascript/bundles/PlayerApp/components/PlayerApp.jsx
+++ app/javascript/bundles/PlayerApp/components/PlayerApp.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from '../store'
 import Resources from './Resources';
 import DevelopmentCards from './DevelopmentCards';
-import "typeface-arvo";
 
 const Name = styled.h1`
   font-family: 'Arvo';
