52c22f79a18a2357e1d5b341dfda5ac3ee304617
Adam Saeid <adam.saeid@gmail.com>
2020-05-25 03:54:38 +0100

    fix api url

--- app/javascript/bundles/PlayerApp/actions/playerActions.js
+++ app/javascript/bundles/PlayerApp/actions/playerActions.js
@@ -12,7 +12,7 @@ export const getPlayerSuccess = (player) => ({
 
 export const getPlayer = (playerId) => async (dispatch) => {
   dispatch(getPlayerLoading())
-  const response = await fetch(`https://gentle-fortress-04729.herokuapp.com/${playerId}`)
+  const response = await fetch(`https://gentle-fortress-04729.herokuapp.com/players/${playerId}`)
   const player = await response.json();
   dispatch(getPlayerSuccess(player));
 }
